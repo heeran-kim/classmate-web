0c964bd35cdeb62006743ce8e4ed5462ed03090d
Jinwoo Kim <jinunemo@gmail.com>
2024-09-17 14:03:37 +0900

    create controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\AssessmentController;
+use App\Http\Controllers\ReviewController;
+
+Route::resource('user', UserController::class);
+Route::resource('course', CourseController::class);
+Route::resource('assessment', AssessmentController::class);
+Route::resource('review', ReviewController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
