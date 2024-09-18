a41ada2ee2de263f23eea36a24b3da912fa9d21e
Heeran Kim <heerankim3@gmail.com>
2024-09-18 20:59:46 +1000

    [minor] assessment create typo, datetime

--- resources/views/assessments/create.blade.php
+++ resources/views/assessments/create.blade.php
@@ -5,7 +5,7 @@
     <p><label>Instruction: </label><textarea name="instruction" rows="10"></textarea></p>
     <p><label>The number of reviews required: </label><input type="number" name="num_required_reviews"></p>
     <p><label>Maximum Score: </label><input type="number" name="max_score"></p>
-    <p><label>Dute Date: </label><input type="date" name="due_date"></p>
+    <p><label>Due Date: </label><input type="datetime-local" name="due_date"></p>
     <p><label>Type: </label><select name="type">
         <option value="student-select">Student Select</option>
         <option value="teacher-assign">Teacher Assign</option>
