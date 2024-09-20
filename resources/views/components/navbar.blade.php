aa206233ec2a24de30c83ba3ca684ab778f7f170
Heeran Kim <heerankim3@gmail.com>
2024-09-20 17:37:51 +1000

    [Bootstrap] navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,28 @@
-@auth
-    {{Auth::user()->name}}
-    <form method="POST" action="{{route('logout')}}">
-        @csrf
-        <input type="submit" value="Logout">
-    </form>
-@else
-    <a href="{{route('login')}}">Log in</a>
-    <a href="{{route('register')}}">Register</a>
-@endauth
+<!-- Navbar for navigating through the app (Listings, Owners, and Create page) -->
+<nav class="shadow-sm">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between py-3 mb-4 border-bottom">
+            <!-- Logo and Home Link -->
+            <div>
+                <a href="{{route("dashboard")}}" class="d-flex align-items-center text-decoration-none">
+                    <img src="{{asset('/images/logo.png')}}" class="me-2" height="32">
+                    <div class="d-none d-sm-block fs-4">ClassMate</div>
+                </a>
+            </div>
+    
+            <div class="d-flex algin-items-center">
+                @auth
+                    <div class="d-flex align-items-center me-2">{{Auth::user()->name}}</div>
+                    <form method="POST" action="{{route('logout')}}">
+                        @csrf
+                        <button type="submit" class="btn btn-outline-primary">Logout</button>
+                    </form>
+                @else
+                    <a href="{{route('login')}}"><button type="button" class="btn btn-outline-primary me-2">Log in</button></a>
+                    <a href="{{route('register')}}"><button type="button" class="btn btn-outline-primary">Register</button></a>
+                @endauth
+            </div>
+        </div>
+    </div>
+</nav>
+
