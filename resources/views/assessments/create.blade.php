ac32bd46417d5fc6aec35c0605746703e7e9f4cf
Heeran Kim <heerankim3@gmail.com>
2024-09-20 20:44:27 +1000

    [minor] text danger

--- resources/views/assessments/create.blade.php
+++ resources/views/assessments/create.blade.php
@@ -3,45 +3,53 @@
         <h3 class="ms-1 mb-3">{{$course->name}} ({{$course->code}})</h3>
         <hr>    
         <h4>Create Assessment</h4>
-        @if (count($errors) > 0)
-        <!-- todo: text in red on alert -->
-            <div class="alert">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         <div class="bg-light p-3 border rounded shadow-sm m-3">
             <form method="POST" action="{{ route('assessment.store') }}" class="row g-3">
                 @csrf
                 <div class="col-12">
                     <label class="form-label">Title</label>
                     <input type="text" class="form-control" name="title" value="{{old('title')}}">
+                    @if ($errors->has('title'))
+                        <small class="text-danger">{{ $errors->first('title') }}</small>
+                    @endif
                 </div>
                 <div class="col-12">
                     <label class="form-label">Instruction</label>
                     <textarea name="instruction" class="form-control" rows="3">{{old('instruction')}}</textarea>
+                    @if ($errors->has('instruction'))
+                        <small class="text-danger">{{ $errors->first('instruction') }}</small>
+                    @endif
                 </div>
                 <div class="col-12 col-md-6">
                     <label class="form-label">The number of reviews required</label>
                     <input type="number" class="form-control" name="num_required_reviews" value="{{old('num_required_reviews')}}">
+                    @if ($errors->has('num_required_reviews'))
+                        <small class="text-danger">{{ $errors->first('num_required_reviews') }}</small>
+                    @endif
                 </div>
                 <div class="col-12 col-md-6">
                     <label class="form-label">Maximum Score</label>
                     <input type="number" class="form-control" name="max_score" value="{{old('max_score')}}">
+                    @if ($errors->has('max_score'))
+                        <small class="text-danger">{{ $errors->first('max_score') }}</small>
+                    @endif
                 </div>
                 <div class="col-12 col-md-6">
                     <label class="form-label">Due Date</label>
                     <input type="datetime-local" class="form-control" name="due_date" value="{{old('due_date')}}">
+                    @if ($errors->has('due_date'))
+                        <small class="text-danger">{{ $errors->first('due_date') }}</small>
+                    @endif
                 </div>
                 <div class="col-12 col-md-6">
                     <label class="form-label">Type</label>
-                    <select name="type" class="form-control">
+                    <select name="type" class="form-select">
                         <option value="student-select" {{old('type') == "student-select" ? 'selected' : ''}}>Student Select</option>
                         <option value="teacher-assign" {{old('type') == "teacher-assign" ? 'selected' : ''}}>Teacher Assign</option>
                     </select>
+                    @if ($errors->has('type'))
+                        <small class="text-danger">{{ $errors->first('type') }}</small>
+                    @endif
                 </div>
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary mt-3">Create</button>
